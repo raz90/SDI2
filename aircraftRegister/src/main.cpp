61b1a04d3f1704cd039ad6f14e15f63ba6ef002a
raz90 <vladmir346@yahoo.co.uk>
2014-04-29 17:49:35 +0800

    add try and catch in dataCon
    try and catch in datacon, add browse function,
    changing maxDiff value,

--- aircraftRegister/src/main.cpp
+++ aircraftRegister/src/main.cpp
@@ -4,17 +4,12 @@
 // Version:
 // Date: 
 // Description:
-
-
-
-
 #include "dataCon.cpp"
 #include "../header/SSK.h"
 #include "../header/SSB.h"
 #include "../header/vessel.h"
 
 int main(int argc, char **argv)
-
 {	
 	
 	DataCon<Vessel> sub1;
@@ -28,19 +23,18 @@ int main(int argc, char **argv)
 	sub1.addLanding("landing",2,3,4,5,6,5,9,10,11,12);
 	sub1.addAirCarrier("carry",2,3,4,5,6,7,9,0);
 	sub1.addDestroyer("destroy",2,3,4,5,6,7,9,10);
+	
 	sub1.observe();
-	//sub1.showVessel();
-	//sub1.deleteVessel("katak");
-	//sub1.findVessel("katak");
+	sub1.showVessel();
+	sub1.deleteVessel("katak");
+	sub1.findVessel("katak");
+	sub1.deleteVessel("katak");
 	//sub1.bubbleSort();
 	//sub1.showVessel();
-	//sub1.save();
+	sub1.save();
+	sub1.load("savefile.txt");
 	//sub1.load("savefile.txt");
 	//sub1.showVessel();
-	
-
-		
-	
 	
 	return 0;
 } 
